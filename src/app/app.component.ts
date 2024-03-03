a019767e58e6b343e0662c588c94e63523f0c353
Isaias Junior <junioors.017@gmail.com>
2024-03-03 11:59:23 -0300

    Atualização dos arquivos: angular.json, package-lock.json, package.json e vários arquivos da pasta src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Componente1Component } from "./componente1/componente1.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, Componente1Component]
 })
 export class AppComponent {
   title = 'projeto-angular';
