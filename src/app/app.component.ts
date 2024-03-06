adc3fb5bbf1182018bb593ac6199a1a5651df0bc
Isaias Junior <junioors.017@gmail.com>
2024-03-06 00:21:09 -0300

    Criada a página de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from "./menu/menu.component";
-import { InicioComponent } from "./inicio/inicio.component";
+import { MenuComponent } from './menu/menu.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MenuComponent, InicioComponent]
+    imports: [RouterOutlet, MenuComponent, InicioComponent, LoginComponent]
 })
 export class AppComponent {
   title = 'Júnior Games';
